0ba7b2a705241138309d22669a71e9f8989b9ce3
TYama <alohaty81@gmail.com>
2022-02-18 11:02:35 -0700

    orders.js done

--- orders.js
+++ orders.js
@@ -30,7 +30,21 @@
 */
 
 //CODE HERE
+class Ticket{
+    constructor(items, orderTime, customerId){
+        this.items = items;
+        this.orderTime = orderTime;
+        this.customerId = customerId;
+        this.status = 'queued';
+    }
 
+    updateStatus(newStatus){
+        this.status = newStatus;
+        console.log(`The order for customer ${this.customerId} is
+        now ${this.status}.`);
+    }
+
+}
 
 
 /*
@@ -45,7 +59,8 @@
 */
 
 //CODE HERE
-
+let firstTicket = new Ticket(['pizza', 'bread', 'soda'], '7:03 PM', 575);
+//console.log(firstTicket);
 
 /*
     Call the `updateStatus` method on
@@ -54,3 +69,5 @@
 */
 
 //CODE HERE
+firstTicket.updateStatus('cooking');
+//console.log(firstTicket);
